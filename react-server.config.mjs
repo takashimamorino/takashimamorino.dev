0aa1f7a11dff711be0b0edf3ac2cab7ea05ee33c
takashimamorino <does1026@gmail.com>
2025-08-26 12:09:12 +0900

    chore: tweak

--- react-server.config.mjs
+++ react-server.config.mjs
@@ -1,3 +1,15 @@
 export default {
   root: "src/app",
+  page: {
+    include: ["**/page.tsx"],
+  },
+  layout: {
+    include: ["**/layout.tsx"],
+  },
+  adapter: [
+    "@lazarv/react-server-adapter-vercel",
+    {
+      edge: true,
+    },
+  ],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import vercelAdapter from "@lazarv/react-server-adapter-vercel";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  adapter: vercelAdapter({
-    serverlessFunctions: false,
-  }),
   plugins: [
     tailwindcss(),
     react({
